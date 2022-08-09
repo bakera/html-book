d0f92f89c0a69a8f10626d05aefb21d259d19fe8
MINAZUKI, Bakera <bakera@star.email.ne.jp>
2022-08-09 10:53:15 +0900

    Revert "2章マークダウン化"

--- epub-scripts/make.sh
+++
@@ -1,2 +0,0 @@
-FILENAME=`git show -s --date=short --format="html-book_%cd_%ct.epub"`
-pandoc ../epub_text/*.md -o $FILENAME --metadata-file=metadata.yaml --epub-cover-image=./cover.png
--- epub-scripts/tohtml.sh
+++
@@ -1,7 +0,0 @@
-for pathname in ../epub_text/*.md
-do
-  echo $pathname
-  MarkdownFileName="${pathname##*/}"
-  HtmlFileName="${MarkdownFileName%.*}.html"
-  pandoc $pathname -o "result/html/$HtmlFileName" --standalone
-done
