005204e3325765d4e8b9a8e3d3149339b64c8419
bakera <bakera@star.email.ne.jp>
2022-10-21 17:10:04 +0900

    XML宣言を追加するようにした

--- epub-scripts/tohtml.sh
+++ epub-scripts/tohtml.sh
@@ -4,6 +4,8 @@ do
   MarkdownFileName="${pathname##*/}"
   HtmlFileName="${MarkdownFileName%.*}.xhtml"
   pandoc $pathname --standalone --template=template.html \
+    | sed 's/<!DOCTYPE html>/<\?xml version="1.0" encoding="UTF-8"\?>\n<!DOCTYPE html>/g' \
+    | sed 's/<meta charset="utf-8" \/>/<meta http-equiv="content-type" content="text\/html; charset=utf-8" \/>/g' \
     | sed 's/role="doc-noteref"/role="doc-noteref" epub:type="noteref"/g' \
     | sed 's/\[\([0-9][0-9]*\)\]/<span class="fignumber"><span class="paren">[<\/span>\1<span class="paren">]<\/span><\/span>/g' \
     | sed 's/<\/figcaption>/<\/figcaption>\n<div class="figbody">/g' \
